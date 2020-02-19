c192469722184aa589998bf1cbb2fa41134a7327
Ben Wadsworth <btwadsworth@gmail.com>
2020-02-19 15:08:05 -0600

    cleaned up

--- weather_api.py
+++ weather_api.py
@@ -1,16 +1,20 @@
 import requests
 import os
+from datetime import datetime
 
 api_key = os.environ.get('WEATHER_KEY')
-print(api_key)
 query = {'q': 'minneapolis,mn,us', 'units': 'imperial', 'appid': api_key}
+
 url = 'http://api.openweathermap.org/data/2.5/forecast'
 
 data = requests.get(url, params=query).json()
 
-
 for item in data['list']:
-    print(item['dt_txt'])
+    timestamp = item['dt']
+    date = datetime.fromtimestamp(timestamp)
+    # Prints the Local Time for temperature
+    # Times printed does not start from time where code is being run.  Starts from location of query.
+    print(date)         
     all_temps = item['main']
     print(f'Temperature: {all_temps["temp"]} F')
     print(f'Description: {item["weather"][0]["description"]}')
